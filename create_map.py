b2b69e28b3e0fc207e13d0fa742559cabc71b46e
datadonK23 <datadonk23@gmail.com>
2019-04-14 20:03:16 +0200

    custom icon

--- create_map.py
+++ create_map.py
@@ -13,7 +13,11 @@ import folium
 m = folium.Map(location=[48.04274, 14.42127], zoom_start=15)
 
 # Mark POIs
-#FIXME
+folium.Marker(
+    location=[48.04274, 14.42127],
+    popup="Info",
+    icon=folium.Icon(color="red", prefix="fa", icon="futbol-o")
+).add_to(m) #FIXME
 
 # Save map
 m.save("index.html")
