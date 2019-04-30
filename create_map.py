069ec07c155a44468ecdd99f50138643f72a9536
datadonK23 <datadonk23@gmail.com>
2019-04-30 22:56:26 +0200

    Added type hints

--- create_map.py
+++ create_map.py
@@ -8,11 +8,18 @@ Date: 14.04.19
 """
 
 import os
-import geopandas as gpd
-import folium
+from typing import List, NewType
+import geopandas as gpd # type: ignore
+import folium # type: ignore
+
+
+# Custom types
+Html = NewType("Html", str)
+
 
 # Basemap
-m = folium.Map(location=[48.04274, 14.42127], zoom_start=15,
+map_center: List[float] = [48.04274, 14.42127]
+m = folium.Map(location=map_center, zoom_start=15,
                tiles="http://tile.stamen.com/toner-background/{z}/{x}/{y}.png",
                attr="style:<a href=http://maps.stamen.com/toner-background/>Stamen toner-background</a>"
                )
@@ -23,27 +30,27 @@ path = "data/"
 f_name = "locations.geojson"
 f_path = os.path.join(path, f_name)
 
-gdf = gpd.read_file(f_path)
+gdf: gpd.GeoDataFrame = gpd.read_file(f_path)
 
 
 # Render information
-def get_info(title, description, photo_url):
+def get_info(title: str, description: str, photo_url: str) -> Html:
     """ Generates HTML code for popup
 
-    :param title:
+    :param title: title text
     :param description: description text < 1001 characters
     :param photo_url: url of photo (max_width=300px, height=200px)
     :return: HTML code snippet
     """
 
-    html = """ 
+    html = Html(""" 
     <!doctype html>
     <html>
     <h1>{}</h1>""".format(title) + """
     <iframe width="310" height="210" src='{}'""".format(photo_url) + """ 
     frameborder="0"></iframe>
     <p>{}</p>""".format(description) + """
-    </html>"""
+    </html>""")
 
     return html
 
@@ -54,10 +61,10 @@ for _, row in gdf.iterrows():
                                   row["photo_url"]), script=True)
     popup = folium.Popup(iframe, parse_html=True, max_width=350)
 
+    loc = [row.geometry.y, row.geometry.x]
     folium.Marker(
-        location=[row.geometry.y, row.geometry.x],
-        tooltip=row["name"],
-        popup=popup,
+        location=loc,
+        tooltip=row["name"], popup=popup,
         icon=folium.Icon(color="red", prefix="fa", icon="futbol-o")
     ).add_to(m)
 
