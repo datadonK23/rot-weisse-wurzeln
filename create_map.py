941a400498bfb1e8320fa42142995ffb07196533
datadonK23 <datadonk23@gmail.com>
2019-09-06 18:02:06 +0200

    Update Teaser texts (WIP). Button in popup.

--- create_map.py
+++ create_map.py
@@ -13,6 +13,7 @@ import geopandas as gpd # type: ignore
 import folium # type: ignore
 from folium import FeatureGroup
 from locate_control import LocateControl
+import validators # type:ignore
 
 
 # Custom types
@@ -113,24 +114,27 @@ fg_walks.add_to(map)
 
 
 # Render information
-def get_info(title: str, description: str, photo_url: str) -> Html:
+def get_info(title: str, description: str, photo_url: str, link: str) -> Html:
     """ Generates HTML code for popup
 
     :param title: title text
     :param description: description text < 1001 characters
     :param photo_url: url of photo (max_width=300px, height=200px)
+    :param link: url for external info page
     :return: HTML code snippet
     """
 
+    # Input validation
     if len(title) > 40:
         raise ValueError("Title string is too long")
     if len(description) > 1000:
         raise ValueError("Description string is too long")
-    if (".jpg" in photo_url) or (".png" in photo_url):
-        pass
-    else:
+    if len(link) > 150:
+        raise ValueError("URL for external page too long")
+    if not validators.url(link):
+        raise ValueError("URL for external page is malformed")
+    if not ((".jpg" in photo_url) or (".png" in photo_url)):
         raise ValueError("Photo URL must point to a .jpg or .png file")
-
 
     html = Html(""" 
     <!doctype html>
@@ -138,7 +142,9 @@ def get_info(title: str, description: str, photo_url: str) -> Html:
     <h1>{}</h1>""".format(title) + """
     <iframe width="284" height="184" src='{}'""".format(photo_url) + """ 
     frameborder="0"></iframe>
-    <p>{}</p>""".format(description) + """
+    <p>{}</p>""".format(description) +
+    """<button onclick="window.open('{}','_blank')" type="button">
+         Mehr dazu</button>""".format(link) + """
     </html>""")
 
     return html
@@ -149,7 +155,7 @@ fg_points: FeatureGroup = FeatureGroup(name="Stationen", show=True)
 
 for _, row in points_gdf.iterrows():
     iframe = folium.Html(get_info(row["name"], row["text"],
-                                  row["photo_url"]), script=True)
+                                  row["photo_url"], row["link"]), script=True)
     popup = folium.Popup(iframe, parse_html=True, max_width=280)
 
     loc = [row.geometry.y, row.geometry.x]
